6e853532bb15d8d8906f0b1eec248abb5198e707
Rafael Ito <rafaelito91@gmail.com>
2017-08-24 20:14:57 -0300

    Section 2: 10. Variable creation, assignment and simple variable coersion String + Number concept. Also, introducing a little of browser native methods.

--- 01/script.js
+++ 01/script.js
@@ -1,4 +1,4 @@
-var name = "John";
+/* var name = "John";
 console.log(name);
 
 var lastName = "Smith";
@@ -8,4 +8,33 @@ var age = 26;
 console.log(age);
 
 var fullAge = true;
-console.log(fullAge);
+console.log(fullAge); */
+
+// Lecture: variables 2
+
+var name = 'John';
+var age = 26;
+
+console.log(name + age);
+console.log(age + age);
+console.log(name + true);
+
+var job, isMarried;
+
+console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+age = 'thirty six'
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is the last name?');
+
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
