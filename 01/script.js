e4a5419a3e6d7fc26fe50004851515f93e579420
Rafael Ito <rafaelito91@gmail.com>
2017-08-24 20:58:33 -0300

    Section 2: Code challenge.

--- 01/script.js
+++ 01/script.js
@@ -1,27 +1,36 @@
-// lecture: Boolean logic and switch statements
+var ageJohn = 24;
+var heightJohn = 172;
 
-var age = 25;
+var ageFriend = 23;
+var heightFriend = 176;
 
-if (age <= 19) {
-  console.log('John is a teenager');
-} else if (age >= 20 && age < 30) {
-  console.log('John is a Young man');
+var ageMark = 25;
+var heightMark = 164;
+
+var johnScore = heightJohn + 4 * ageJohn;
+var friendScore = heightFriend + 4 * ageFriend;
+var markScore = heightMark + 4 * ageMark;
+
+console.log('John Score: ' + johnScore);
+console.log('Friend Score: ' + friendScore);
+console.log('Mark Score:' + markScore);
+
+// First part of challenge
+if (johnScore > friendScore) {
+  alert('John Wins!!!')
+} else if (johnScore === friendScore) {
+  alert('Its a DRAW!')
 } else {
-  console.log('John is a man');
+  alert('John loses! :( )')
 }
 
-var job = prompt('What does John do?');
-
-switch(job) {
-  case 'teacher':
-    console.log('John teaches kids');
-    break;
-  case 'driver':
-    console.log('John Driver a Cab in Lisbon');
-    break;
-  case 'cop':
-    console.log('John fights crime');
-    break;
-  default:
-    console.log('John does something else');
+//Second part
+if (johnScore > friendScore && johnScore > markScore) {
+  alert('John Wins!!!')
+} else if (friendScore > johnScore && friendScore > markScore) {
+  alert('Friend wins')
+} else if(markScore > johnScore && markScore > friendScore) {
+  alert('Mark wins')
+} else {
+  alert('IT\'S A DRAW!!!!!')
 }
