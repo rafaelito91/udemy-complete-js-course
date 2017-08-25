3672c8ca493506f4fc6dbf9f04bf0fd39dc3959d
Rafael Ito <rafaelito91@gmail.com>
2017-08-24 23:53:49 -0300

    Section 2: 20 part 2: Using 'this' keyword inside js objects in order to use their own properties inside its methods (internal functions)

--- 01/script.js
+++ 01/script.js
@@ -1,12 +1,5 @@
 // Lecture: Objects and methods
 
-// The functtion in the object below is basically the same as:
-/*
-var calculateAge = function(yearOfBirth) {
-
-}
-*/
-
 var john = {
   name : 'John',
   lastName : 'Smith',
@@ -14,21 +7,22 @@ var john = {
   job : 'teacher',
   isMarried : false,
   family: ['Jane', 'Mark', 'Bob'],
-  //Why would a provide yearOfBirth as parameter if the object has it as attribute... so...
-  //calculateAge: function(yearOfBirth) {
-  //  return 2017 - yearOfBirth;
-  //}
   calculateAge: function() {
-    return 2017 - this.yearOfBirth;
+    this.age = 2017 - this.yearOfBirth;
+  }
+};
+console.log(john); // Why is age being added to the log in console if
+// at this point the calculateAge method had not been called?
+john.calculateAge();
+console.log(john);
+
+
+var mike = {
+  yearOfBirth: 1950,
+  calculateAge: function() {
+    this.age = 2017 - this.yearOfBirth;
   }
 };
 
-console.log(john);
-console.log(john.family[2]);
-// console.log(john.calculateAge(1990));  No need for it anymore
-console.log(john.calculateAge());
-
-var age = john.calculateAge();
-john.age = age;
-
-console.log(john);
+mike.calculateAge();
+console.log(mike);
