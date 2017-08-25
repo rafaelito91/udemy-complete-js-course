ec2fcbccae402975dbae83f9045c55693c06013b
Rafael Ito <rafaelito91@gmail.com>
2017-08-25 00:23:33 -0300

    Section 2: Code challenger number 2.

--- 01/script.js
+++ 01/script.js
@@ -1,34 +1,45 @@
 // Loops
 
-/*
-for (var i = 0; i < 10; i++) {
-  console.log(i);
-}
-*/
-
-var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
-
-/*
-for (var i = 0; i < names.length; i++) {
-  console.log(names[i]);
+function calculateAge(yearOfBirth) {
+  var actualYear = 2017;
+  return actualYear - yearOfBirth;
 }
 
-for (var i = names.length - 1; i >= 0 ; i--) {
-  console.log(names[i]);
+// Exercise 1
+var yearsOfBirth = new Array(1990, 1956, 1970, 2011, 1950);
+
+// Exercise 2
+var arrayOfAges = new Array();
+
+// Exercise 3
+for (var i = 0; i < yearsOfBirth.length ; i++) {
+  arrayOfAges.push(yearsOfBirth[i]);
 }
 
-var i = 0;
-while (i < names.length) {
-  console.log(names[i]);
-  i++;
-}
-*/
-for (var i = 0; i < 5; i++) {
-  if (i === 2) {
-    continue;
-  }
-  console.log(i);
-  if (i === 3) {
-    break;
+// Exercise 4
+for (var i = 0; i < arrayOfAges.length ; i++) {
+  if (calculateAge(arrayOfAges[i]) > 18) {
+    console.log('Is full age');
+  } else {
+    console.log('Is under age');
   }
 }
+
+// Exercise 5
+var printFullAge = function(yearsOfBirth) {
+  var fullAgeArray = new Array();
+  for (var i = 0; i < yearsOfBirth.length ; i++) {
+    if (calculateAge(yearsOfBirth[i]) > 18) {
+      fullAgeArray.push(true);
+    } else {
+      fullAgeArray.push(false);
+    }
+  }
+  return fullAgeArray;
+}
+
+var full_1 = printFullAge([1965, 2008, 1992]);
+var full_2 = printFullAge([1951, 1991, 2016]);
+
+console.log(full_1);
+console.log(full_2);
