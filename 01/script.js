48b74f0cc3863f43f817d3fabc5216beb89f4295
Rafael Ito <rafael.ito@focusnetworks.com.br>
2017-08-25 22:33:10 -0300

    Section 3: Scope !important

--- 01/script.js
+++ 01/script.js
@@ -1,47 +1,27 @@
-//Lecture: Hoisting
-/*
-  It was explained in the lecture that at the start of the executing
-  phase, all js variables and functions are declared. However, only
-  the functions are actually assigned, which means that the variables
-  will be created with 'undefined' value.
-
-  In the example below, the function is being called before its
-  declaration, but the example works fine in the console. And that
-  is due to the 'hoisting'.
-*/
-calculateAge(1999);
-
-function calculateAge(year) {
-  console.log(2017 - year);
-}
-
+//Lecture: Scope
 
 /*
-  Now it won't work! Why? Because its not a function declaration, it
-  is a function expression. THAT IS ASSIGNED TO A VARIABLE!!! Which
-  means that its hoisting will only assign "undefined" as the variable
-  value.
+  This access to parent scopes is called scope chain. THere is a chain
+  of scopes that are accessed from the child to its parent.
 */
-//retirement(1990);
 
-var retirement = function(year) {
-  console.log(65 - (2017 - year));
+
+
+// Global scope
+var a = 'Hello!';
+first();
+
+// Local scopes
+function first() {
+  // first function scope
+  // dont have access to variable c, but has access to variable a and b
+  var b = 'Hi!';
+  second();
+
+  function second() {
+    // second function scope
+    // has access to variables a b and c
+    var c = 'Hey!';
+    console.log (a + b + c);
+  }
 }
-
-
-/* This will be undefined */
-//console.log(age);
-/* This will throw an error due to the fact that the variable is
-not even declared */
-//console.log(abdula);
-var age = 23;
-
-console.log(age);
-
-function foo() {
-  console.log(age); // undefined -> PQ? pq nesse contexto da pilha de execucao, ainda nao foi inciada a variabel
-  var age = 64; // SE essa declaracao for comentada, age aqui terá o valor da variavel age global.
-  console.log(age);
-}
-foo();
-console.log(age);
