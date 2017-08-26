575cbabe60fda8ce21795e74dd200abf1e56ee09
Rafael Ito <rafael.ito@focusnetworks.com.br>
2017-08-25 23:07:50 -0300

    Section 3: This variable example number two - 'this' variable assignment is only done when the method is called!. Other concepts: Method borrowing. !important

--- 01/script.js
+++ 01/script.js
@@ -1,34 +1,28 @@
 //Lecture: this keyword
 
-/* First example: this keyword in a regular function declaration*/
-/*
-console.log(this);
-
-calculateAge(1985);
-
-function calculateAge(year) {
-  console.log(2017 - year);
-  console.log(this);
-}
-*/
-
-/* Second example: this keyword in a method*/
 var john = {
   name : 'John',
   yearOfBirth: 1990,
   calculateAge: function() {
     console.log(this);
     console.log(2017 - this.yearOfBirth);
-
-    function innerFunction() {
-      console.log(this);
-      // this will print the window object, because its the rule:
-      // a regular function call will call window, and this is a
-      // regular funcion declaration, even though it is inside a
-      // method
-    }
-
-    innerFunction();
   }
 }
 john.calculateAge();
+
+
+var mike = {
+  name : 'Mike',
+  yearOfBirth: 1951
+}
+
+// Method borrowing
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
+
+/*
+  This shows that the 'this' variable is only assigned
+  when the method is called. If it wasn't like that, 'this'
+  would always be john object, and the method borrowing wouldn't
+  be possible.
+*/
