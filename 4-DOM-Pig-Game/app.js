f64478cc3ce8c93b42f7125c930c5b6fca7d629a
Rafael Ito <rafael.ito@focusnetworks.com.br>
2017-08-27 16:36:25 -0300

    Section 4: Finishing touch - "state variable"... Global variable to control the state of the game. Don't know how to feel about this. Doesn't look very good, but right know i don't have an answer to this without using a framework or something.

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -8,44 +8,49 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 // Outros exemplos de evento : https://developer.mozilla.org/en-US/docs/Web/Events
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  // 1. Random number
-  var dice = Math.floor(Math.random() * 6) + 1;
+  if (gamePlaying) {
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
 
-  // 2. Display result
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  diceDOM.src = 'dice-' + dice + '.png';
+    // 2. Display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
-  // 3. Update de round score unless the result is 1
-  if (dice !== 1) {
-    roundScore += dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore;
-  } else {
-    nextPlayer();
+    // 3. Update de round score unless the result is 1
+    if (dice !== 1) {
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      nextPlayer();
+    }
   }
 });
 
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-  // Add  current score to global score
-  scores[activePlayer] += roundScore;
+  if(gamePlaying) {
+    // Add  current score to global score
+    scores[activePlayer] += roundScore;
 
-  // update the ui
-  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-  // check if player won the game
-  if (scores[activePlayer] >= 20) {
-    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-    document.querySelector('.dice').style.display = 'none';
-    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-  } else {
-    nextPlayer();
+    // update the ui
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    // check if player won the game
+    if (scores[activePlayer] >= 20) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }
   }
 });
 
@@ -56,7 +61,8 @@ function init() {
   scores = [0, 0];
   activePlayer = 0;
   roundScore = 0;
-
+  gamePlaying = true;
+  
   document.querySelector('.dice').style.display = 'none';
 
   document.getElementById('score-0').textContent = '0';
