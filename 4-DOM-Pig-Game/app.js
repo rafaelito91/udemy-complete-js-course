3cf26935acdd1eeb969930b9088e40bf76c7feea
Rafael Ito <rafael.ito@focusnetworks.com.br>
2017-08-27 15:55:43 -0300

    Section 4 - 42: Extract method for nextPlayer function (example of the DRY - don't repeat yourself programming principle). Finishing touches to the application game. Tips about not change style in javascript, the best way to do it is by adding and removing classes. !important

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -12,7 +12,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -33,22 +33,40 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
   // 3. Update de round score unless the result is 1
   if (dice !== 1) {
-    // add score
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
-    // next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-    //document.querySelector('.player-0-panel').classList.remove('active');
-    //document.querySelector('.player-0-panel').classList.add('active');
-
-    document.querySelector('.dice').style.display = 'none';
+    nextPlayer();
   }
-
 });
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  // Add  current score to global score
+  scores[activePlayer] += roundScore;
+
+  // update the ui
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+  // check if player won the game
+  if (scores[activePlayer] >= 20) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  } else {
+    nextPlayer();
+  }
+});
+
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
