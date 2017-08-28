9b570f35ca7ff39a3f70945a57ac166fd4b34929
Rafael Ito <rafael.ito@focusnetworks.com.br>
2017-08-28 00:06:28 -0300

    Section 5: 52 - Creating objects and attaching functions to its prototype in order to show inheritance working. !important

--- 01/script.js
+++ 01/script.js
@@ -1,1 +1,33 @@
-console.log('Hello World!')
+// Function constructor
+
+/*
+var john = {
+  name : 'John',
+  yearOfBirth : 1990,
+  job: 'teacher'
+};
+*/
+
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+  console.log(2017 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1949, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
